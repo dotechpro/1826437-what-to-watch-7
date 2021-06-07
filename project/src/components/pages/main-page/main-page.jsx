347f5d7bb7b30883a71d9b9aad1c449bd709ef84
Wereteychenko Sergey <zergy@list.ru>
2021-06-07 22:26:27 +0300

    logo component

--- project/src/components/pages/main-page/main-page.jsx
+++ project/src/components/pages/main-page/main-page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MovieListItem from '../../ui/movie-list-item/movie-list-item';
+import Logo from '../../ui/logo/logo';
 
 function MainPage({name, genre, released}) {
   return (
@@ -13,13 +14,7 @@ function MainPage({name, genre, released}) {
         <h1 className="visually-hidden">WTW</h1>
 
         <header className="page-header film-card__head">
-          <div className="logo">
-            <a href="/#" className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo/>
 
           <ul className="user-block">
             <li className="user-block__item">
